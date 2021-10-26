bfeab97fe709f2ce5ab33b682ee7b0e301490958
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-27 01:42:16 +0330

    Contact Me

--- js/app.js
+++ js/app.js
@@ -10,3 +10,14 @@ $(".filter-button-group").on("click", "button", function () {
   var filterValue = $(this).attr("data-filter");
   $grid.isotope({filter: filterValue});
 });
+
+// Rest Form ===============================================
+
+const inputs = document.querySelectorAll(".input");
+const resetBtn = document.querySelector(".reset-btn");
+
+resetBtn.addEventListener("click", () => {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+});
